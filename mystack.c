1a03462e19ddc7dc3217c68f1ff7980ef82d61da
Lameck Kipruto <lameckkipruto1@.com>
2023-09-22 10:43:31 -0700

    stack

---
+++ mystack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - func printing the top
+ * @head: stack head
+ * @counter: line number
+ * Return: none
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
